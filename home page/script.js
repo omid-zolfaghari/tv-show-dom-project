c8ccb4e2a73751ea730f45d4dede00fc47ff6937
omid-zolfaghari <omid1380.oz@gmail.com>
2024-03-06 14:58:29 +0330

    ..

--- home page/script.js
+++ home page/script.js
@@ -50,7 +50,6 @@ const movies = async(names, row)=>{
     card.append(image, title, rate, genre)
     row.appendChild(col3)
     
-    // console.log(window.location.href + "/episod.html/");
     card.addEventListener("click", ()=>{
        console.log(data.id);
         setData(data.id);
